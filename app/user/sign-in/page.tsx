cb2a6f17c5e0cd76578450942f3c14fdec14c63b
RahelSiddiqi <azizsiddiqirahel@gmail.com>
2024-02-23 00:00:39 +0600

    checking background issue

--- app/user/sign-in/page.tsx
+++ app/user/sign-in/page.tsx
@@ -1,120 +1,128 @@
-"use client";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
-import Button from "@/components/buttons/Button";
-import { HiArrowRight } from "react-icons/hi";
-import Modal from "@/components/modal/Modal";
-import Input from "@/components/input/InputBox";
-import OTPField from "@/components/input/OTPfield";
-import NewPasswordInput from "@/components/input/NewPasswordInput";
+'use client';
+import Button from '@/components/buttons/Button';
+import Input from '@/components/input/InputBox';
+import NewPasswordInput from '@/components/input/NewPasswordInput';
+import OTPField from '@/components/input/OTPfield';
+import Modal from '@/components/modal/Modal';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
+import { HiArrowRight } from 'react-icons/hi';
 // import backgroundImg from '../../../public/assets/images/signin/bg-signin.png'
 
-export default function Page({ classes }) {
-  const router = useRouter();
-  const [open, setOpen] = useState<boolean>(false);
-  const [getCodeRequested, setGetCodeRequested] = useState<boolean>(false);
-  const [otpValidated, setOtpValidated] = useState<boolean>(false);
+export default function Page({}) {
+	const router = useRouter();
+	const [open, setOpen] = useState<boolean>(false);
+	const [getCodeRequested, setGetCodeRequested] = useState<boolean>(false);
+	const [otpValidated, setOtpValidated] = useState<boolean>(false);
 
-  const handleValidateOTP = () => {
-    // logic to validate OTP
-    // If OTP is valid, switch to the new password input view
-    setOtpValidated(true);
-  };
+	const handleValidateOTP = () => {
+		// logic to validate OTP
+		// If OTP is valid, switch to the new password input view
+		setOtpValidated(true);
+	};
 
-  const handleSetNewPassword = (newPassword: string) => {
-    // logic to save the new password
-    console.log("New Password set:", newPassword);
-  };
+	const handleSetNewPassword = (newPassword: string) => {
+		// logic to save the new password
+		console.log('New Password set:', newPassword);
+	};
 
-  function handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    router.push("/dashboard");
-  }
+	function handleSubmit(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		router.push('/dashboard');
+	}
 
-  return (
-    <main className="bg-signin">
-      <div className="lg:flex">
-        <div className="flex-grow">
-          <div className="p-8 lg:p-16 max-w-screen-lg lg:mt-20">
-            <p>
-              <Link
-                href="/"
-                className="mt-4 uppercase text-3xl lg:text-4xl xl:text-5xl font-extrabold tracking-tight text-white sm:text-4xl"
-              >
-                Guideasy
-              </Link>
-            </p>
-            <p className="mt-4 text-lg leading-8 text-white">
-              Discover the vast world of Guideasy, your gateway to over 300,000
-              products worldwide. Join our global community and explore a
-              universe of choices and proud to be our valuable partner.
-            </p>
+	return (
+		<main className='bg-signin'>
+			<div className='lg:flex'>
+				<div className='flex-grow'>
+					<div className='p-8 lg:p-16 max-w-screen-lg lg:mt-20'>
+						<p>
+							<Link
+								href='/'
+								className='mt-4 uppercase text-3xl lg:text-4xl xl:text-5xl font-extrabold tracking-tight text-white sm:text-4xl'>
+								Guideasy
+							</Link>
+						</p>
+						<p className='mt-4 text-lg leading-8 text-white'>
+							Discover the vast world of Guideasy, your gateway to
+							over 300,000 products worldwide. Join our global
+							community and explore a universe of choices and
+							proud to be our valuable partner.
+						</p>
 
-            <Button classes="py-3 px-6 flex gap-3">Become Our Partner <div className="mt-1"><HiArrowRight /></div> </Button> 
+						<Button classes='py-3 px-6 flex gap-3'>
+							Become Our Partner{' '}
+							<div className='mt-1'>
+								<HiArrowRight />
+							</div>{' '}
+						</Button>
+					</div>
+				</div>
+				<div className='flex-shrink-0 w-full lg:max-w-md bg-white min-h-screen grid place-items-center relative'>
+					<div className='w-full overflow-hidden max-w-md rounded-lg py-12 lg:py-16 px-12 text-center'>
+						<div className='mb-10 text-center md:mb-12'>
+							<h1 className='text-2xl md:text-3xl lg:text-4xl font-semibold'>
+								Sign In
+							</h1>
+						</div>
+						<form className='w-full' onSubmit={handleSubmit}>
+							<Input type='text' placeholder='Email' />
+							<Input type='password' placeholder='Password' />
+							<div className='flex justify-end'>
+								<small
+									className='font-medium text-gray-800 text-sm hover:underline cursor-pointer'
+									onClick={() => {
+										setOpen(true);
+										setGetCodeRequested(false); // Reset the code request state when modal is opened
+									}}>
+									Forgot password?
+								</small>
+							</div>
 
-          </div>
-        </div>
-        <div className="flex-shrink-0 w-full lg:max-w-md bg-white min-h-screen grid place-items-center relative">
-          <div className="w-full overflow-hidden max-w-md rounded-lg py-12 lg:py-16 px-12 text-center">
-            <div className="mb-10 text-center md:mb-12">
-              <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold">
-                Sign In
-              </h1>
-            </div>
-            <form className="w-full" onSubmit={handleSubmit}>
-              <Input type="text" placeholder="Email" />
-              <Input type="password" placeholder="Password" />
-              <div className="flex justify-end">
-                <small
-                  className="font-medium text-gray-800 text-sm hover:underline cursor-pointer"
-                  onClick={() => {
-                    setOpen(true);
-                    setGetCodeRequested(false); // Reset the code request state when modal is opened
-                  }}
-                >
-                  Forgot password?
-                </small>
-              </div>
+							<Modal open={open} onClose={() => setOpen(false)}>
+								{getCodeRequested ? (
+									otpValidated ? (
+										<NewPasswordInput
+											onSetNewPassword={
+												handleSetNewPassword
+											}
+										/>
+									) : (
+										<OTPField
+											onValidateOTP={handleValidateOTP}
+										/>
+									)
+								) : (
+									<>
+										<p className='text-md font-bold'>
+											Enter your email address
+										</p>
+										<div className='grid grid-cols-4 gap-3 mt-5'>
+											<Input
+												type='text'
+												placeholder='Email'
+												classes='col-span-3'
+											/>
+											<Button
+												classes='px-10 py-0 h-12 text-sm'
+												onClick={() =>
+													setGetCodeRequested(true)
+												}>
+												Get Code
+											</Button>
+										</div>
+									</>
+								)}
+							</Modal>
 
-              <Modal open={open} onClose={() => setOpen(false)}>
-                {getCodeRequested ? (
-                  otpValidated ? (
-                    <NewPasswordInput onSetNewPassword={handleSetNewPassword} />
-                  ) : (
-                    <OTPField onValidateOTP={handleValidateOTP} />
-                  )
-                ) : (
-
-                    <>
-                    <p className="text-md font-bold">
-                      Enter your email address
-                    </p>
-                    <div className="grid grid-cols-4 gap-3 mt-5">
-                      <Input
-                        type="text"
-                        placeholder="Email"
-                        classes="col-span-3"
-                      />
-                      <Button
-                        classes="px-10 py-0 h-12 text-sm"
-                        onClick={() => setGetCodeRequested(true)}
-                      >
-                        Get Code
-                      </Button>
-                    </div>
-                  </>
-
-                )}
-              </Modal>
-
-            
-
-              <div className="my-16">
-                <Button classes="py-3 px-10 text-sm">Sign In</Button>
-              </div>
-            </form>
-            {/* <p className="mb-6 text-base text-gray-700">Connect With</p>
+							<div className='my-16'>
+								<Button classes='py-3 px-10 text-sm'>
+									Sign In
+								</Button>
+							</div>
+						</form>
+						{/* <p className="mb-6 text-base text-gray-700">Connect With</p>
                         <ul className="mb-12 flex justify-between">
                             <li className="w-full px-2">
                                 <a href="" className="flex h-11 items-center justify-center rounded-md bg-[#4064AC] hover:bg-opacity-90">
@@ -137,29 +145,27 @@ export default function Page({ classes }) {
                                 </a>
                             </li>
                         </ul> */}
-            <p className="text-gray-800">
-              Not a partner?{" "}
-              <Link
-                href="/user/sign-up"
-                className="mb-2 inline-block text-base text-green-500 hover:text-primary hover:underline"
-              >
-                Become a partner now
-              </Link>
-              
-            </p>
-          </div>
-          <img
-            src="/assets/images/signin/top-ellipse.png"
-            alt="ellipse"
-            className="w-32 lg:w-40 absolute top-0 right-0"
-          />
-          <img
-            src="/assets/images/signin/bottom-ellipse.png"
-            alt="ellipse"
-            className="w-32 lg:w-40 absolute left-0 bottom-0"
-          />
-        </div>
-      </div>
-    </main>
-  );
+						<p className='text-gray-800'>
+							Not a partner?{' '}
+							<Link
+								href='/user/sign-up'
+								className='mb-2 inline-block text-base text-green-500 hover:text-primary hover:underline'>
+								Become a partner now
+							</Link>
+						</p>
+					</div>
+					<img
+						src='/assets/images/signin/top-ellipse.png'
+						alt='ellipse'
+						className='w-32 lg:w-40 absolute top-0 right-0'
+					/>
+					<img
+						src='/assets/images/signin/bottom-ellipse.png'
+						alt='ellipse'
+						className='w-32 lg:w-40 absolute left-0 bottom-0'
+					/>
+				</div>
+			</div>
+		</main>
+	);
 }
