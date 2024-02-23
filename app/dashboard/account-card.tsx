763eeb41f4b241e73b7de5a6c228747cfb3f044b
nafisreza <bigganinafis@gmail.com>
2024-02-24 02:02:53 +0600

    Credit Card information dynamic

--- app/dashboard/account-card.tsx
+++ app/dashboard/account-card.tsx
@@ -1,6 +1,10 @@
-import RecentTransections from "./recent-transection";
+import RecentTransections from "./recent-transactions";
 
 const AccountCard = () => {
+    const accountNumber = '3475 7381 3759 ****'
+    const balance = '3,422,424'
+    const currency = 'USD'
+    const expDate = '04 / 24'
     return (
         <div className="w-full bg-green-500/10 rounded-lg px-2 py-6 flex flex-col lg:flex-row xl:flex-col gap-4">
             <div className="rounded-lg space-y-4 px-4">
@@ -11,16 +15,16 @@ const AccountCard = () => {
                 <div className="text-white ">
                     <div className="bg-green-500 p-4 rounded-t-lg">
                         <h2 className="text-white font-semibold tracking-wider">GUIDEASY</h2>
-                        <h2 className="text-white my-5">3475 7381 3759 ****</h2>
+                        <h2 className="text-white my-5">{accountNumber}</h2>
                     </div>
                     <div className="bg-green-400 flex justify-between p-4 items-end rounded-b-lg">
                         <div>
                             <p className="text-[10px] text-gray-50/70">TOTAL BALANCE</p>
-                            <p className="text-sm">3,422,424 USD</p>
+                            <p className="text-sm">{balance} {currency}</p>
                         </div>
                         <div>
                             <p className="text-[10px] text-gray-50/70">EXP. DATE</p>
-                            <p className="text-sm">04 / 24</p>
+                            <p className="text-sm">{expDate}</p>
                         </div>
                     </div>
                 </div>
