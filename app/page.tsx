56d33b6c2edba5efe0d609e4c1b8a97d09c62e20
nafisreza <bigganinafis@gmail.com>
2024-02-24 20:26:43 +0600

    Home background updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import Navbar from "./_home/home-navbar";
-import BG from '../assets/images/home/bg-home.png'
+import BG from "../assets/images/home/bg-home.png";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-home+ min-h-screen relative">
-      <Image src={BG} alt="background" layout="fill" objectFit="cover" className="absoute w-full left-0 top-0" />
-      <div className="relative">
+    <main className="min-h-screen relative"  style={{
+      backgroundImage: `url("../assets/images/home/bg-home.png")`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat'
+    }}>
         <Navbar />
-      </div>
+
     </main>
-  )
+  );
 }
