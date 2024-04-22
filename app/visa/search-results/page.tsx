af18b37eabca79b53451299c49dc1a130519f8f2
nafisreza <bigganinafis@gmail.com>
2024-04-22 22:19:26 +0600

    Visa search results - api integration

--- app/visa/search-results/page.tsx
+++ app/visa/search-results/page.tsx
@@ -4,10 +4,36 @@ import App from "../../app"
 import ContainerMain from "@/components/container/main"
 import VisaSearchCard from "@/components/card/flight/VisaSearchCard"
 import VisaSearchSummary from "@/components/search-form/VisaSearchSummary";
+import axios from "axios";
+import { useEffect } from "react";
 
 type Props = {}
 
 const Page: React.FC<Props> = () => {
+
+    useEffect(() => {
+        const fetchVisas = async () => {
+          try {
+            const response = await axios.get(`http://endorse.guideasy.com/api/v1/client-management/packages/9aedbe05-ffe8-42f9-a1df-d0982b605845/products/9aedb861-2ca1-47db-b02d-a630351bcb36`, {
+              headers: {
+                Authorization: "Bearer 354|SRmsDVJRGG7gE6nPDNptMUgAFvnXxtRWMP1J9V9aeac014f2",
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                "Accept-Language": "en",
+                'X-App-Latitude': 23.6101974,
+                'X-App-Longitude': 90.4597919,
+                'X-App-Currency': 'BDT'
+              },
+            });
+            console.log(response.data.payload)
+          } catch (error) {
+            console.error("Error fetching visas:", error);
+          }
+        };
+    
+        fetchVisas();
+      }, []);
+
     return (
         <App>
             <VisaSearchSummary />
--- components/card/flight/VisaSearchCard.tsx
+++ components/card/flight/VisaSearchCard.tsx
@@ -1,54 +1,101 @@
-'use client'
+import Link from "next/link";
+import RangeSlider from "@/components/range-slider/range-slider";
+import { FaStar } from "react-icons/fa6";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import { ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import { countries } from '../../../lib/countries'
-import RangeSlider from '@/components/range-slider/range-slider'
-import Link from 'next/link'
-import { SelectCountry } from '@/components/input/select/CountrySelect'
+export type Props = { link?: string };
 
-export type Props = { link?: string }
+export const CardItem: React.FC<Props> = ({ link, visa }) => {
+  return (
+    <Link
+      href={link || "details"}
+      className="flex flex-wrap justify-between items-center p-4 bg-white text-gray-800 rounded-xl space-y-3 md:space-y-6 shadow"
+    >
+      <div className="flex justify-center items-center gap-4">
+        <img
+          src={visa.image}
+          alt="Visa"
+          className="w-32 object-cover"
+        />
+        <div>
+          <h5 className="text-lg font-medium">{visa.title}</h5>
+          <p className="text-md">{visa.products[0].category}</p>
+          <p className="text-xs">{visa.products[0].company}</p>
+        </div>
+      </div>
 
-export const CardItem: React.FC<Props> = ({ link }) => {
-    return (
-        <Link href={link || "details"} className="flex flex-wrap justify-between items-center p-4 bg-white text-gray-800 rounded-xl space-y-3 md:space-y-6 shadow">
-            <figure className="flex gap-3 items-center">
-                <img src="/assets/images/flight.png" alt="profile" className="w-16 md:w-20 h-24 md:h-28 object-cover" />
-                <div className='space-y-1'>
-                    <h5 className="text-base font-medium">Thynk Travels</h5>
-                    <p className="text-sm">Travel Visa</p>
-                </div>
-            </figure>
-            <div className='flex flex-col sm:items-end gap-1'>
-                <p className="text-sm">Start From</p>
-                <h5 className="text-base font-medium">Case of Hit &amp; Run</h5>
-                <p className="text-sm">Earn BDT 400</p>
-            </div>
-        </Link>
-    )
-}
+      <div className="right flex flex-col items-end">
+        <div className="flex">
+          <FaStar color="#FFD700" />
+          <p className="text-gray-700 font-xs mt-[-2px]">{visa.products[0].rating}</p>
+        </div>
+        <h5 className="text-base">{visa.products[0].entrance} Entry</h5>
+        <del className="text-red-500">
+          <span className="text-gray-500 font-light text-sm">
+            {visa.pricing.maximum.str}
+          </span>
+        </del>
+        <p className="font-semibold text-md">{visa.pricing.minimum.str}</p>
+      </div>
+    </Link>
+  );
+};
 
 export default function VisaSearchCard() {
-    return (
-        <section className="space-y-2">
-            <div className="flex flex-col xl:flex-row items-start gap-6">
-                <form className="flex-shrink-0 w-72 bg-white rounded-xl shadow space-y-4">
-                    <h3 className='text-lg font-semibold p-4 border-b'>Filter</h3>
-                    <div className="flex flex-col items-center gap-6 px-4 lg:px-6 py-8">
-                        <RangeSlider />
-                        {/* <div className="w-full"><SelectCountry /></div> */}
-                        <button type="submit" className="text-white bg-green-500 hover:bg-green-600 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center gap-2">Modify</button>
-                    </div>
-                </form>
-                <div className="w-full flex-grow grid grid-cols-1 lg:grid-cols-2 gap-4">
-                    <CardItem />
-                    <CardItem />
-                    <CardItem />
-                    <CardItem />
-                    <CardItem />
-                </div>
-            </div>
-        </section>
-    )
+  const [visas, setVisas] = useState([]);
+
+  const dispatch = useDispatch();
+  const visaCountry = useSelector((state: any) => state.location.visaCountry);
+  const nationality = useSelector((state: any) => state.location.nationality);
+  const visaType = useSelector((state) => state.visaType.visaTypes); 
+  console.log(visaType[0]?.id)
+
+  useEffect(() => {
+    const fetchVisas = async () => {
+      try {
+        const response = await axios.get(`http://endorse.guideasy.com/api/v1/client-management/packages?[country]=${visaCountry?.id || ''}&filter[nationality]=${nationality?.id || ''}&filter[category]=${visaType[0]?.id || ''}&filter[entrance]=s`, {
+          headers: {
+            Authorization: "Bearer 354|SRmsDVJRGG7gE6nPDNptMUgAFvnXxtRWMP1J9V9aeac014f2",
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Accept-Language": "en",
+            'X-App-Latitude': 23.6101974,
+            'X-App-Longitude': 90.4597919,
+            'X-App-Currency': 'BDT'
+          },
+        });
+        setVisas(response.data.payload ? [response.data.payload] : []);
+      } catch (error) {
+        console.error("Error fetching visas:", error);
+      }
+    };
+
+    fetchVisas();
+  }, []);
+
+  return (
+    <section className="space-y-2">
+      <div className="flex flex-col xl:flex-row items-start gap-6">
+        <form className="flex-shrink-0 w-72 bg-white rounded-xl shadow space-y-4">
+          <h3 className="text-lg font-semibold p-4 border-b">Filter</h3>
+          <div className="flex flex-col items-center gap-6 px-4 lg:px-6 py-8">
+            <RangeSlider />
+            <button
+              type="submit"
+              className="text-white bg-green-500 hover:bg-green-600 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center gap-2"
+            >
+              Modify
+            </button>
+          </div>
+        </form>
+        <div className="w-full flex-grow grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {visas.map((visa, index) => (
+            <CardItem key={index} visa={visa} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
