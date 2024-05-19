357ba3e92668082cd001b724ab59752ea055cdc8
nafisreza <bigganinafis@gmail.com>
2024-05-20 03:16:32 +0600

    Garbage redux files removed

--- app/react-redux/constants/action-types.js
+++
@@ -1,4 +0,0 @@
-export const ActionTypes = {
-    SET_VISA_COUNTRY: "SET_VISA_COUNTRY",
-    SET_VISA_NATIONALITY: "SET_VISA_NATIONALITY"
-}
--- app/react-redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { visaReducer } from "../../store/visaReducer";
-
-const reducers = combineReducers({
-    visa: visaReducer
-})
-
-export default reducers;
